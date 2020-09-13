25ed7ba7d8fb1378e53f152a19432038c410d74e
JussiVJ <jusku39@gmail.com>
2020-09-13 22:48:39 +0300

    Update scsi-disk.c

--- hw/scsi/scsi-disk.c
+++ hw/scsi/scsi-disk.c
@@ -2382,7 +2382,7 @@ static void scsi_realize(SCSIDevice *dev, Error **errp)
         s->version = g_strdup(qemu_hw_version());
     }
     if (!s->vendor) {
-        s->vendor = g_strdup("QEMU");
+        s->vendor = g_strdup("ASUS");
     }
     if (!s->device_id) {
         if (s->serial) {
@@ -2438,7 +2438,7 @@ static void scsi_hd_realize(SCSIDevice *dev, Error **errp)
     s->qdev.blocksize = s->qdev.conf.logical_block_size;
     s->qdev.type = TYPE_DISK;
     if (!s->product) {
-        s->product = g_strdup("QEMU HARDDISK");
+        s->product = g_strdup("WD HARDDRIVE");
     }
     scsi_realize(&s->qdev, errp);
 out:
@@ -2467,7 +2467,7 @@ static void scsi_cd_realize(SCSIDevice *dev, Error **errp)
     s->qdev.type = TYPE_ROM;
     s->features |= 1 << SCSI_DISK_F_REMOVABLE;
     if (!s->product) {
-        s->product = g_strdup("QEMU CD-ROM");
+        s->product = g_strdup("ASUS CD");
     }
     scsi_realize(&s->qdev, errp);
     aio_context_release(ctx);
